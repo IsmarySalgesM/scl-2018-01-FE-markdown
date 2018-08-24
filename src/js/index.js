331a8cb1f17294da1dc800ddba66d5501cb1569a
NadiaQN <quezada.nadia@gmail.com>
2018-08-24 10:37:17 -0300

    test arriba

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,6 @@
 #!/usr/bin/env node
-//const path = require("path");
-const fs = require('fs');
-const [, , ...args] = process.argv;
+const args = process.argv;
+console.log(args);
 
-//console.log(path);
-//process.argv.forEach((val, index) => {
-// console.log(`${index}: ${val}`);
-//});
-let prueba = fs.readFileSync("README.md");
-console.log(prueba);
 
-//fs.readFileSync();
-
-//}
-//}
-//});
 
