7e55962ac00963cec8288e9c372128a691ead6f1
IsmarySalgesM <ismarysalges@gmail.com>
2018-08-27 12:03:34 -0300

    comentarios nuevos

--- src/js/index.js
+++ src/js/index.js
@@ -9,17 +9,16 @@ const [, , ...prop] = process.argv;
 let absPath = prop[0];
 
 if (!path.isAbsolute(absPath)) {
-  let absPathtry = process.cwd(absPath);
-  let prueba = path.join(absPathtry, prop[0]);
+  let absPathtry = process.cwd(absPath); // ubica la carpeta de directorio
+  let prueba = path.join(absPathtry, prop[0]); // une carpeta directorio con la busqueda del usuario
   //console.log(prueba);
   takePath(prueba);
 }
-
+ // buscamos el archivo 
 function takePath(ruta) {
     fs.readFile(`${ruta}`, 'utf-8', (err, data) => {
       if (err) throw err;
       console.log(data);
     });
 }
-
 
