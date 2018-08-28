1f1cf1b9002cbb9da745d277c3c0284b92097b70
IsmarySalgesM <ismarysalges@gmail.com>
2018-08-28 11:01:59 -0300

    eureka con promesas

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,23 @@
 #! /usr/bin/env node
-const markdownLinkExtractor = require('./../../src/js/prueba');
+const takePath = require('./../../src/js/prueba');
 const path = require('path');
-const fs = require('fs');
 
-// buscamos el archivo 
-takePath = (ruta) => {
-  fs.readFile(`${ruta}`, 'utf-8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-    markdownLinkExtractor(data);
-  });
-}
+
 
 //operacion de esparcir
+
 const [, , ...prop] = process.argv;
 
-let absPath = prop[0];
+let absPath = prop[0]
+
 
 if (!path.isAbsolute(absPath)) {
   let absPathtry = process.cwd(absPath); // ubica la carpeta de directorio
   let prueba = path.join(absPathtry, prop[0]); // une carpeta directorio con la busqueda del usuario
-  //console.log(prueba);
+  let options = {};
+  if (prop.includes('--validate')) options.validate = true;
   takePath(prueba);
 }
-
 
 
 
