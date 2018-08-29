e6484c600e96039c6bfc7d9cffe8341575341012
IsmarySalgesM <ismarysalges@gmail.com>
2018-08-29 19:39:40 -0300

    color out

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
-const takePath = require('./../../src/js/prueba');
+const takePath = require('./../../src/js/extractLinks');
 const path = require('path');
 const colors = require('colors');
 
@@ -18,19 +18,20 @@ if (!path.isAbsolute(absPath)) {
 function mdlinks() {
   let absPathtry = process.cwd(absPath); // ubica la carpeta de directorio
   let concaPath = path.join(absPathtry, absPath);
-      takePath(concaPath).then((values)=>{
-        values.forEach(element => {
-       let linksContent = `Link : ${colors.blue(element.href)} Titulo:${colors.white(element.title)} Status:${colors.red(element.status)} StatusOk:${colors.red(element.statusOk)}`
-        console.log(linksContent) 
-        });
-        
-      }
-    )}
+  takePath(concaPath).then((values) => {
+    values.forEach(element => {
+      let linksContent = `Link : ${colors.blue(element.href)} Titulo:${colors.yellow(element.title)} Status:${colors.red(element.status)} StatusOk:${colors.red(element.statusOk)}`
+      console.log(linksContent)
+    });
+
+  }
+  )
+}
       //forEach((elemento, index)=>{})
    // }).catch((error) => {
   //    console.error("Error > " + error); 
   //  })
-  
+
   //let options = {};
   //if (prop.includes('--validate')) options.validate = true;
 
